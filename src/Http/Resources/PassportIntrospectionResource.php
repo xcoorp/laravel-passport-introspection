d84176bd0fbbc26ce2bdf14a05932ec4ddd0ee93
Tobias Oitzinger <tobias@xcoorp.com>
2024-09-04 11:41:28 +0200

    fix(introspection): fix returning the username
    Signed-off-by: Tobias Oitzinger <tobias@xcoorp.com>

--- src/Http/Resources/PassportIntrospectionResource.php
+++ src/Http/Resources/PassportIntrospectionResource.php
@@ -20,7 +20,7 @@ class PassportIntrospectionResource extends JsonResource
         return [
             'active' => $this->when(isset($this['active']), fn () => $this['active']),
             'client_id' => $this->when(isset($this['client_id']), fn () => $this['client_id']),
-            'username' => $this->when(isset($this['user_id']), fn () => $this['username']),
+            'username' => $this->when(isset($this['username']), fn () => $this['username']),
             'scope' => $this->when(isset($this['scope']), fn () => $this['scope']),
             'exp' => $this->when(isset($this['exp']), fn () => $this['exp']),
         ];
