68a5c2376828626d81809e6e6150290be96832ae
Tobias Oitzinger <tobias@xcoorp.com>
2025-05-09 13:10:44 +0200

    fix(introspection): fix introspection result
    mark inactive, when expired, set correct sub if client credentials, fix evaluating the credential_type
    
    Signed-off-by: Tobias Oitzinger <tobias@xcoorp.com>

--- src/Factories/IntrospectionResponseFactory.php
+++ src/Factories/IntrospectionResponseFactory.php
@@ -24,22 +24,24 @@ class IntrospectionResponseFactory implements IntrospectionResponseFactoryContra
             ]);
         }
 
+        $client = $accessToken?->client;
+        $type = match (true) {
+            $client?->personal_access_client => CredentialType::PersonalAccess,
+            $client?->password_client => CredentialType::Password,
+            !$client?->secret => CredentialType::PKCE,
+            $client->redirect === '' => CredentialType::ClientCredentials,
+            $client?->redirect !== null && $client->redirect !== '' => CredentialType::AuthorizationCode,
+            default => CredentialType::Unknown,
+        };
+
         /** @noinspection PhpUndefinedFieldInspection */
         return new PassportIntrospectionResource([
-            'active' => $token->revoked === false,
+            'active' => $token->revoked === false && $token->expires_at?->isFuture() && $token->created_at?->isPast(),
             'client_id' => $accessToken?->client_id,
             'username' => $accessToken?->user?->email ? $accessToken->user->email : null,
-            'sub' => $accessToken?->user_id,
+            'sub' => $type === CredentialType::ClientCredentials ? $accessToken?->client_id : $accessToken?->user_id,
             'scope' => implode(' ', $accessToken?->scopes),
-            'credential_type' => $accessToken?->client?->personal_access_client ? CredentialType::PersonalAccess : (
-                $accessToken?->client?->password_client ? CredentialType::Password : (
-                    !$accessToken?->client?->secret ? CredentialType::PKCE : (
-                        $accessToken?->client?->redirect && $accessToken->client->redirect === '' ? CredentialType::ClientCredentials : (
-                            $accessToken?->client?->redirect && $accessToken->client->redirect !== '' ? CredentialType::AuthorizationCode : CredentialType::Unknown
-                        )
-                    )
-                )
-            ),
+            'credential_type' => $type,
             'exp' => $token?->expires_at?->getTimestamp(),
             'iat' => $token?->created_at?->getTimestamp(),
             'nbf' => $token?->created_at?->getTimestamp(),
